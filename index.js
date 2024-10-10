e77798df7b4677d7b596961bdee8c3d941e3c46a
Shambhuraj Dhanawade <shambhurajpdhanawade@gmail.com>
2024-10-10 17:22:54 +0530

    Update index.js

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ const db = new pg.Client({
   host: process.env.PG_HOST,
   database: process.env.PG_DATABASE,
   password: process.env.PG_PASSWORD,
-  // port: process.env.PG_PORT,
+  port: process.env.PG_PORT || 5432,
 });
 db.connect();
 
